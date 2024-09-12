c5336cfd3812bf42bb3ece6e58f01e90b4ec1f60
code043 <procoder043@gmail.com>
2024-09-12 02:48:08 -0300

    config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import dotenv from "dotenv";
 import { routerTours } from "./infra/http/express/routes/routes";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routerTours);
