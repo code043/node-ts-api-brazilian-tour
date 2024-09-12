fbb08543c55c7bda98c0a302f307cb8eb78fe52b
code043 <procoder043@gmail.com>
2024-09-12 03:22:09 -0300

    Config deploy

--- prisma/migrations/20240911011647_tour/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Tour" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "info" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-
-    CONSTRAINT "Tour_pkey" PRIMARY KEY ("id")
-);
